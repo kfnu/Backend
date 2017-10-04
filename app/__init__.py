cc3abfb6967e6495b55a4eeb8358676cd09effd9
root <root@ip-172-31-6-230.us-east-2.compute.internal>
2017-10-04 23:55:52 +0000

    Remove unwanted files from git

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
