5511bc4870d31b7bae86261c009f862e465fa068
Noah Ignacio Smith <noahignaciosmith@gmail.com>
2017-10-05 19:27:58 -0400

    Added user creation, user deletion, user info editing, and user info viewing (#19)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from sqlalchemy.orm import validates
+from datetime import datetime
 
 class User(db.Model):
     """
@@ -8,15 +10,30 @@ class User(db.Model):
     __tabelname_ = 'Users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), nullable=False)
-    email = db.Column(db.String(60), nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
     birthday = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, id, username, email, birthday):
-        self.id = id
+    def __init__(self, username, email, birthday):
         self.username = username
         self.email = email
         self.birthday = birthday
+
+    @validates('username')
+    def validate_username(self, key, username):
+        assert db.session.query(User).filter_by(username=username) is not None, "Username taken"
+        return username
+
+    @validates('email')
+    def validate_email(self, key, email):
+        assert '@' in email, 'Invalid email'
+        return email
+
+    @validates('birthday')
+    def validate_birthday(self, key, birthday):
+        assert datetime.strptime(birthday, '%Y/%m/%d'), "Invalid date"
+        return birthday
+
 
     def __repr__(self):
         return '<User id: {}, Username: {}, Email: {}, Birthday: {}>'.format(self.id, self.username, self.email, self.birthday)
